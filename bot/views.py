27c7879e7be01b7ef2171b1ee139eefe16a98bba
vgalimov <vgalimov@emphasoft.com>
2020-12-03 16:53:18 +0300

    start game

--- bot/views.py
+++ bot/views.py
@@ -6,12 +6,17 @@ from django.http import HttpResponse
 from django.views.generic import View
 
 from .models import UserBot
+from .utils import shuffle_users
 
 
 def send_mess(chat, text):
     params = {'chat_id': chat, 'text': text}
     response = requests.post(settings.BOT_URL + 'sendMessage', data=params)
     return response
+
+
+def check_if_user_admin(username):
+    return username == 'iselbst'
 
 
 class BotWebhook(View):
@@ -36,6 +41,11 @@ class BotWebhook(View):
                 send_mess(telegram_id, 'Поздравляю, вы участвуете в игре!')
             else:
                 send_mess(telegram_id, 'Вы уже зарегистрированы, дождитесь других участников.')
+        elif text == '/start_game' and check_if_user_admin(username):
+            users = UserBot.objects.all()
+            pairs = shuffle_users(list(users))
+            for pair in pairs:
+                send_mess(pair[0].telegram_id, f'Твоя цель: {pair[1]}! Удачи :)')
         else:
             send_mess(telegram_id, 'Пока что мой функционал очень ограничен :(')
 
