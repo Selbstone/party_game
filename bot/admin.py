cd9edc67e8a0df40a134760f0e4812c6e69f04a0
vgalimov <vgalimov@emphasoft.com>
2020-12-04 00:58:02 +0300

    refactoring

--- bot/admin.py
+++ bot/admin.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.http import HttpResponseRedirect
+from django.urls import re_path
 
 from .models import UserBot
 from .utils import send_goals_to_users
@@ -13,7 +13,7 @@ class UserBotAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(UserBotAdmin, self).get_urls()
         custom_urls = [
-            url('^start_game/$', self.start_game, name='start_game'),
+            re_path('^start_game/$', self.start_game, name='start_game'),
         ]
         return custom_urls + urls
 
--- bot/views.py
+++ bot/views.py
@@ -18,32 +18,41 @@ class BotWebhook(View):
 
         user_chat = body['message']['chat']
         text = body['message']['text']
-        first_name = user_chat['first_name']
-        username = user_chat['username']
-        telegram_id = user_chat['id']
+        self.first_name = user_chat['first_name']
+        self.username = user_chat['username']
+        self.telegram_id = user_chat['id']
 
         if text == '/start':
-            if not UserBot.objects.filter(telegram_id=telegram_id).exists():
-                UserBot.objects.create(
-                    first_name=first_name,
-                    username=username,
-                    telegram_id=telegram_id
-                )
-                send_mess(telegram_id, 'Поздравляю, вы участвуете в игре!')
-            else:
-                send_mess(telegram_id, 'Вы уже зарегистрированы, дождитесь других участников.')
+            self.start()
         elif text == '/start_game':
-            if check_if_user_admin(username):
-                send_goals_to_users()
-            else:
-                send_mess(telegram_id, 'Только админ может начать игру.')
+            self.start_game()
         elif text == '/party':
-            users = UserBot.objects.all()
-            party = ''
-            for user in users:
-                party = party + f'{str(user)}\n'
-            send_mess(telegram_id, party)
+            self.party()
         else:
-            send_mess(telegram_id, 'Пока что мой функционал очень ограничен :(')
+            send_mess(self.telegram_id, 'Пока что мой функционал очень ограничен :(')
 
         return HttpResponse(True)
+
+    def start(self):
+        if not UserBot.objects.filter(telegram_id=self.telegram_id).exists():
+            UserBot.objects.create(
+                first_name=self.first_name,
+                username=self.username,
+                telegram_id=self.telegram_id
+            )
+            send_mess(self.telegram_id, 'Поздравляю, вы участвуете в игре!')
+        else:
+            send_mess(self.telegram_id, 'Вы уже зарегистрированы, дождитесь других участников.')
+
+    def start_game(self):
+        if check_if_user_admin(self.username):
+            send_goals_to_users()
+        else:
+            send_mess(self.telegram_id, 'Только админ может начать игру.')
+
+    def party(self):
+        users = UserBot.objects.all()
+        party = ''
+        for user in users:
+            party = party + f'{str(user)}\n'
+        send_mess(self.telegram_id, party)
